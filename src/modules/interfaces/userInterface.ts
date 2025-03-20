fdb78b768240a311117d7e0ab86a1c492b452c23
Thmsantos <thiagomessias411@gmail.com>
2025-03-20 11:15:14 -0300

    :green_heart: feat: Adicionando interface e classe de usuário

---
+++ src/modules/interfaces/userInterface.ts
@@ -0,0 +1,9 @@
+export interface User{
+    id: string //trocar por objectId
+    usuário: string
+    nome: string
+    email: string
+    senha: string
+    status: boolean
+    calculos: string[] //cálculo[]
+}
