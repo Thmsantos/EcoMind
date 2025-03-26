0a29385ac845702cb50f70061a31a25fd49fd51b
thmsantos <thiagomessias411@gmail.com>
2025-03-26 11:21:52 -0300

    :zap: feat: Conectando com o banco de dados

--- app.ts
+++ app.ts
@@ -1,8 +1,24 @@
 import express from 'express';
-
+import { MongoClient } from "mongodb";
 const app = express();
 const port = 2010;
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+function connectToMongoDB() {
+    const mongoURI = 'mongodb://localhost:27017';
+    let dbClient: MongoClient | null  = new MongoClient(mongoURI);
+    dbClient = new MongoClient(mongoURI);
+    dbClient.connect();
+    console.log("Conectado ao banco");
+}
+
+async function execute() {
+  await connectToMongoDB();
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+execute();
+
+
