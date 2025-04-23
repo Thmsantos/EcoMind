707b1819ef002e3053bdcdb238db43ea032ccdf9
Marigsiqueira <mgsmariana123@gmail.com>
2025-04-23 00:54:55 -0300

    :sparkling_heart: feat: add service, controller, and repository for Calculo

--- src/api/routes/route.ts
+++ src/api/routes/route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import userRoutes from "../controllers/User/index.js";
 import rankingRoutes from "../controllers/Ranking/index.js"
+import calculoRoutes from "../controllers/Calculo/index.js"
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/calculos", calculoRoutes);
 app.use("/api/ranking", rankingRoutes);
 
 export default app;
---
+++ src/modules/Calculo/repository/CalculoRepository.ts
@@ -0,0 +1,23 @@
+import { ObjectId } from "mongodb";
+import { client } from "../../../config/db.js";
+import { CalculoInterface } from "../interfaces/calculoInterface.js";
+
+class CalculoRepository{
+    public async createCalculo(calculos: CalculoInterface): Promise<void> {
+        try {
+          const db = client.db("EcoMind");
+          const collection = db.collection<CalculoInterface>("calculo");
+    
+          const result = await collection.insertOne(calculos);
+          console.log("Calculo inserido com sucesso:", result); 
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            throw error;
+          }
+    
+          throw new Error(String(error));
+        }
+      }
+}
+
+export default CalculoRepository;
