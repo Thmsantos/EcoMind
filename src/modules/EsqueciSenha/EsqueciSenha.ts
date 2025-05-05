5869681e329b64d161fa816f48abdc3e48c97157
thiagomessias411@gmail.com <Thiagomessias411@gmail.com>
2025-05-05 11:04:23 -0300

    :recycle: refactor: Trocando a classe EsqueciSenha de diretório

--- src/modules/EsqueciSenha/EsqueciSenha.ts
+++ src/modules/EsqueciSenha/EsqueciSenha.ts
@@ -2,23 +2,40 @@ import { ObjectId } from "mongodb";
 
 class EsqueciSenha {
   private id: ObjectId;
+
   private userId: ObjectId;
+
   private senhasAntigas: string[];
+
+  private codigo: number;
+  
   private senhaAtual: string;
 
-  constructor(userId: ObjectId, senhaAtual: string) {
+  private createdAt: Date;
+
+  constructor(userId: ObjectId, senhaAtual: string, codigo: number) {
     this.id = new ObjectId();
     this.userId = userId;
+    this.codigo = codigo;
     this.senhasAntigas = [];
     this.senhaAtual = senhaAtual;
+    this.createdAt = new Date();
   }
 
   public getId(): ObjectId {
     return this.id;
   }
 
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
   public getUserId(): ObjectId {
     return this.userId;
+  }
+
+  public getCodigo(): number {
+    return this.codigo;
   }
 
   public getSenhasAntigas(): string[] {
@@ -33,12 +50,20 @@ class EsqueciSenha {
     this.id = id;
   }
 
+  public setCreatedAt(date: Date): void {
+    this.createdAt = date;
+  }
+
+  public setCodigo(codigo: number): void {
+    this.codigo = codigo
+  }
+
   public setUserId(userId: ObjectId): void {
     this.userId = userId;
   }
 
-  public setSenhasAntigas(senhasAntigas: string[]): void {
-    this.senhasAntigas = senhasAntigas;
+  public setSenhasAntigas(senhaAntiga: string): void {
+    this.senhasAntigas.push(senhaAntiga);
   }
 
   public setSenhaAtual(senhaAtual: string): void {
@@ -49,3 +74,5 @@ class EsqueciSenha {
     this.senhasAntigas.push(senha);
   }
 }
+
+export default EsqueciSenha;
