1e92470f9fbf1bb5fab658205f1f6b6edf0f1cb7
thiagomessias411@gmail.com <Thiagomessias411@gmail.com>
2025-05-05 11:07:09 -0300

    :sparkles: feat: Adicionando end point do modulo de Esqueci Senha

--- src/api/routes/route.ts
+++ src/api/routes/route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "../controllers/User/index.js";
 import rankingRoutes from "../controllers/Ranking/index.js"
 import calculoRoutes from "../controllers/Calculo/index.js"
+import esqueciSenhaRoutes from "../controllers/EsqueciSenha/index.js";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/calculos", calculoRoutes);
 app.use("/api/ranking", rankingRoutes);
+app.use("/api/esqueciSenha", esqueciSenhaRoutes);
 
 export default app;
