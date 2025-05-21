ffadb387c8f6dffbd71b3f9ed1afdb26977682c0
Thmsantos <Thiagomessias411@gmail.com>
2025-05-21 11:06:58 -0300

    :zap: feat: Implementando cors

--- src/api/routes/route.ts
+++ src/api/routes/route.ts
@@ -3,10 +3,11 @@ import userRoutes from "../controllers/User/index.js";
 import rankingRoutes from "../controllers/Ranking/index.js";
 import calculoRoutes from "../controllers/Calculo/index.js";
 import esqueciSenhaRoutes from "../controllers/EsqueciSenha/index.js";
-
 import { errorHandler } from "../../config/midlleware/errorHandler.js"; 
+import cors from "cors"
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
