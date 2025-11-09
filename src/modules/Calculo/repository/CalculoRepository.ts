48acfb15bf29c01a86e0ad0641ac870765246660
thmsantos <Thiagomessias411@gmail.com>
2025-11-09 15:30:17 -0300

    :wastebasket: chore: Clean up calc repository

--- src/modules/Calculo/repository/CalculoRepository.ts
+++ src/modules/Calculo/repository/CalculoRepository.ts
@@ -1,28 +1,15 @@
+import { InsertOneResult } from "mongodb";
 import { client } from "../../../config/database/db.ts";
 import type { CalculoInterface } from "../interfaces/CalculoInterface.ts";
 
 class CalculoRepository {
-  public async createCalculo(calculos: CalculoInterface): Promise<void> {
-    try {
-      const db = client.db("EcoMind");
-      const collection = db.collection<CalculoInterface>("calculo");
-
-      await collection.insertOne(calculos);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        throw error;
-      }
-
-      throw new Error(String(error));
-    }
-  }
-
-  public async searchCalculo(pip: any){
-    const db = client.db("Ecomind");
+  public async createCalculo(calculos: CalculoInterface): Promise<InsertOneResult<CalculoInterface> | null> {
+    const db = client.db("EcoMind");
     const collection = db.collection<CalculoInterface>("calculo");
-  
-    const calculo = await collection.aggregate<CalculoInterface>(pip).next();
-    return calculo ?? null;
+
+    const createdCalculo = await collection.insertOne(calculos);
+
+    return createdCalculo;
   }
 }
 
