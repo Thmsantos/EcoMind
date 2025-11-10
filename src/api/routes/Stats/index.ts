bbcbb655adb50bb210f50c58087b2323f7604294
Thmsantos <thiagomessias411@gmail.com>
2025-11-10 20:44:40 -0300

    :recycle: refactor: Upgrade calc endpoint

--- src/api/routes/Stats/index.ts
+++ src/api/routes/Stats/index.ts
@@ -4,6 +4,6 @@ import StatsController from "../../controllers/Stats/index.ts";
 const statsRoutes = Router();
 const statsController = new StatsController();
 
-statsRoutes.get("/:idUser", (req, res) => statsController.searchStats(req, res))
+statsRoutes.get("/:id", (req, res) => statsController.searchStats(req, res))
 
 export default statsRoutes;
