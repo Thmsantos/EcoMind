4ae68a53eca87e1706e41050c90746418ad3dc23
thmsantos <Thiagomessias411@gmail.com>
2025-11-10 11:06:27 -0300

    :sparkles: feat: add login in routes

--- src/api/routes/User/index.ts
+++ src/api/routes/User/index.ts
@@ -9,5 +9,6 @@ userRoutes.get("/:id", (req, res) => userController.searchUser(req, res));
 userRoutes.post("/create", (req, res) => userController.createUser(req, res));
 userRoutes.put("/:id", (req, res) => userController.updateUser(req, res))
 userRoutes.delete("/:id", (req, res) => userController.deleteUser(req, res))
+userRoutes.post("/login", (req, res) => userController.login(req, res));
 
 export default userRoutes;
